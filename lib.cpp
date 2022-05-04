0915dfd45c54234f3491dc018d3e0fbc8212ab6d
5254416 <100807837+5254416@users.noreply.github.com>
2022-05-04 16:20:50 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 
 bool funzione (int n){
-    if (((65<=n) and (n<90)) or ((97<=n) and (n<=122))){
+    if (((65<=n) and (n<=90)) or ((97<=n) and (n<=122))){
         return true;
     }else{
         return false;
