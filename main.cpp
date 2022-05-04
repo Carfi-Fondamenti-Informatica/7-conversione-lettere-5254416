c03ff805fe946ec2842f8239014359380e792a2e
5254416 <100807837+5254416@users.noreply.github.com>
2022-05-04 16:11:49 +0200

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@
 using namespace std;
 
 int main(){
-    char c;
-    cin >> c;
-    int n = (int) c;
-    bool a = funzione(n);
+   char c;
+   cin >> c;
+   int n = (int) c;
+   bool x = funzione(n);
 
-    if (a==0){
-        cout << "errore";
-    }else if (a==1){
-        if ((65<=n)&&(n<=90)){
-            n=n+32;
-            cout <<(char)n<<endl;
-        } else if ((97<=n)&&(n<=122)){
-            n=n-32;
-            cout<<(char)n<< endl;
-        }
-    }
-
-
+   if (x==0){
+      cout<<"errore";
+   }else if (x==1){
+      if((65 <= n)&&(n <= 90)){
+         n=n+32;
+         cout <<(char)n;
+      } else if ((97 <=n)&&(n<=122)){
+         n=n-32;
+         cout<<(char)n;
+      }
+   } 
+   
+          
     return 0;
 }
