2993f356b855098d5f61a35ddc6d2c6ca39da14e
5254416 <100807837+5254416@users.noreply.github.com>
2022-05-04 15:44:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+  char a;
+  cin>>a;
+  if(controllo(a))
+  {
+   cout<<a;
+  }
+  else
+  {
+      cout<<"errore";
+  }
+    return 0;
 }
