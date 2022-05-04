44c45ea4db3f93241cb6095e1ec16c498b731737
5254416 <100807837+5254416@users.noreply.github.com>
2022-05-04 15:48:19 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,21 +1,10 @@
 #include "lib.h"
-bool controllo(char & b)
-{
-    int a=(int)b;
-    if(a>=65 && a<=90)
-    {
-        a=a+32;
-        b=(char)a;
+
+
+bool funzione(int n){
+    if (((65<=n)&&(n<=90)) || ((97<=n) && (n<=122))){
         return true;
-    }
-    else if(a>=97 && a<=122)
-    {
-        a=a-32;
-        b=(char)a;
-        return true;
-    }
-    else
-    {
+    }else{
         return false;
     }
 }
